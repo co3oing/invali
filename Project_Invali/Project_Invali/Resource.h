7e54ee132f3134cc16eb71d0e42dd8bf8fbf46ee
Liberty <gigacms@gmail.com>
2018-12-02 22:56:50 +0900

    Revert "Revert "test""
    This reverts commit 95fd667da3f7fc9c08d1fe139320e12620b28634.

--- Project_Invali/Project_Invali/Resource.h
+++ Project_Invali/Project_Invali/Resource.h
@@ -33,6 +33,7 @@
 #define IDC_TREE_CONTROL                1000
 #define IDC_EXPLORER1                   1001
 #define IDC_EXPLORER                    1001
+#define IDC_LIST1                       1004
 
 // Next default values for new objects
 // 
@@ -40,7 +41,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        312
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
