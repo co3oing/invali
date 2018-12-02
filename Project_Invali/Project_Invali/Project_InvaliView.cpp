ffad5a4ce5d7d355642c34f3162e7b2a7e642fed
Liberty <gigacms@gmail.com>
2018-12-02 18:44:18 +0900

    Project creat

---
+++ Project_Invali/Project_Invali/Project_InvaliView.cpp
@@ -0,0 +1,98 @@
+
+// Project_InvaliView.cpp : CProject_InvaliView 클래스의 구현
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Project_Invali.h"
+#endif
+
+#include "Project_InvaliDoc.h"
+#include "Project_InvaliView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CProject_InvaliView
+
+IMPLEMENT_DYNCREATE(CProject_InvaliView, CFormView)
+
+BEGIN_MESSAGE_MAP(CProject_InvaliView, CFormView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CProject_InvaliView 생성/소멸
+
+CProject_InvaliView::CProject_InvaliView()
+	: CFormView(IDD_PROJECT_INVALI_FORM)
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CProject_InvaliView::~CProject_InvaliView()
+{
+}
+
+void CProject_InvaliView::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+}
+
+BOOL CProject_InvaliView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CFormView::PreCreateWindow(cs);
+}
+
+void CProject_InvaliView::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+	GetParentFrame()->RecalcLayout();
+	ResizeParentToFit();
+
+}
+
+void CProject_InvaliView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CProject_InvaliView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CProject_InvaliView 진단
+
+#ifdef _DEBUG
+void CProject_InvaliView::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+void CProject_InvaliView::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+
+CProject_InvaliDoc* CProject_InvaliView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CProject_InvaliDoc)));
+	return (CProject_InvaliDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CProject_InvaliView 메시지 처리기
---
+++ Project_Invali/Project_Invali/Project_InvaliView.h
@@ -0,0 +1,55 @@
+
+// Project_InvaliView.h : CProject_InvaliView 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CProject_InvaliView : public CFormView
+{
+protected: // serialization에서만 만들어집니다.
+	CProject_InvaliView();
+	DECLARE_DYNCREATE(CProject_InvaliView)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_PROJECT_INVALI_FORM };
+#endif
+
+// 특성입니다.
+public:
+	CProject_InvaliDoc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+	virtual void OnInitialUpdate(); // 생성 후 처음 호출되었습니다.
+
+// 구현입니다.
+public:
+	virtual ~CProject_InvaliView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // Project_InvaliView.cpp의 디버그 버전
+inline CProject_InvaliDoc* CProject_InvaliView::GetDocument() const
+   { return reinterpret_cast<CProject_InvaliDoc*>(m_pDocument); }
+#endif
+
