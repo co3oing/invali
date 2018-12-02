0507b43e17c56225a0cfcc53f3c2800a9bccaad5
JAHYNE <JAHYNE@LAPTOP-U403UHQ0>
2018-12-02 22:59:36 +0900

    시펄

--- Project_Invali/Project_Invali/Resource.h
+++ Project_Invali/Project_Invali/Resource.h
@@ -31,17 +31,17 @@
 #define ID_VIEW_APPLOOK_WINDOWS_7       219
 #define IDS_EDIT_MENU                   306
 #define IDC_TREE_CONTROL                1000
-#define IDC_EXPLORER1                   1001
 #define IDC_EXPLORER                    1001
 #define IDC_LIST1                       1004
+#define IDC_SPIN1                       1005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        312
+#define _APS_NEXT_RESOURCE_VALUE        316
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
